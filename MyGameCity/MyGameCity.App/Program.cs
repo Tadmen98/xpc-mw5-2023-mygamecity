37476dd7fd9ddd7a703d34d62954d7e9e698b662
Tadeas Navratil <tadeas.navratil@kolibrik.net>
2023-03-06 14:45:00 +0100

    add empty solution

---
+++ MyGameCity/MyGameCity.App/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MyGameCity.App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

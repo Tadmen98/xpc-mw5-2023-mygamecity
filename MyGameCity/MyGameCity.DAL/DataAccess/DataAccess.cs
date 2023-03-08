9ed900775df0c737675ce5a17eee8f3ebf820b32
Tadeas Navratil <tadeas.navratil@kolibrik.net>
2023-03-08 15:02:57 +0100

    Create repository Interface

---
+++ MyGameCity/MyGameCity.DAL/DataAccess/DataAccess.cs
@@ -0,0 +1,22 @@
+﻿using MyGameCity.DAL.Entities;
+using MyGameCity.DAL.Data;
+using MyGameCity.DAL.Migrations;
+
+namespace MyGameCity.DAL.DataAccess;
+
+public class DataAccess
+{
+    GamesDbContext context = new GamesDbContext();
+
+    public void AddDbEntry<TEntity>(TEntity entity) where TEntity : EntityBase
+    {
+        this.context.Add(entity);
+    }
+
+    public void RemoveEntry<TEntity>(TEntity entity) where TEntity : EntityBase
+    {
+        this.context.Remove(entity);
+    }
+
+    public void SaveChanges() => this.context.SaveChanges(); 
+}
---
+++ MyGameCity/MyGameCity.DAL/Repositories/IRepository.cs
@@ -0,0 +1,11 @@
+﻿using MyGameCity.DAL.Entities;
+
+namespace MyGameCity.DAL.Repositories;
+
+public interface IRepository<TEntity> where TEntity : class, IEntity
+{
+    Guid Create(TEntity entity);
+    TEntity GetById(Guid id);
+    TEntity Update(TEntity entity);
+    void Delete(Guid id);
+}

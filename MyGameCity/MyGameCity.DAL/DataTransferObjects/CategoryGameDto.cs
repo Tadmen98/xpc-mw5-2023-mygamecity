329ed775dbd3c7325a1b88f953a1e1f98dccfc96
Tadeas Navratil <tadeas.navratil@kolibrik.net>
2023-04-07 23:51:31 +0200

    feature: add category game dto object

---
+++ MyGameCity/MyGameCity.DAL/DataTransferObjects/CategoryGameDto.cs
@@ -0,0 +1,14 @@
+﻿using MyGameCity.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyGameCity.DAL.DataTransferObjects;
+
+public record CategoryGameDto
+{
+    public Guid CategoryId { get; set; }
+    public Guid SkillId { get; set; }
+}

d6a31c4ec6c1b242fb7dea9d28f62298693ccbd5
Tadeas Navratil <tadeas.navratil@kolibrik.net>
2023-04-07 13:00:51 +0200

    feat: add DataContext class just for one item

---
+++ MyGameCity/MyGameCity.DAL/Data/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using MyGameCity.DAL.Entities;
+
+namespace MyGameCity.DAL.Data;
+
+public class GamesDbContext : DbContext
+{
+    public DbSet<CategoryEntity> Categories { get; set; }
+    public DbSet<ComodityEntity> Comodity { get; set; }
+    public DbSet<ManufacturerEntity> Manufacturer { get; set; }
+    public DbSet<ReviewEntity> Review { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        //base.OnConfiguring(optionsBuilder);
+        optionsBuilder.UseSqlite("Data Source=C:\\Users\\TAD\\Desktop\\VUT\\10 semestr\\XPC-MW5\\xpc-mw5-2023-mygamecity\\MyGameCity\\MyGameCity.DAL\\Database\\games.db");
+    }
+    //TODO: remove harcoded path
+    //TODO: replace with variable databaseName
+}

4827c912199bd26f096c0bbbe14067016794fb2f
Adam Zaujec <247504@vutbr.cz>
2023-04-07 20:52:20 +0200

    update commint

--- MyGameCity/MyGameCity/DataModel/Review.cs
+++ MyGameCity/MyGameCity/DataModel/Review.cs
@@ -5,8 +5,8 @@ namespace MyGameCity.DataModel
     public class Review
     {
         public int Stars { get; set; }
-        public string Title { get; set; }
-        public string Description { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
         public override string ToString()
         {
             return JsonSerializer.Serialize(this, new JsonSerializerOptions { WriteIndented = true });

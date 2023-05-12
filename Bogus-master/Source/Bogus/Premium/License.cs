5c12c93ce4adbb6da5dc20bf27b9bb29075af9dc
Pukako <125278977+Pukako@users.noreply.github.com>
2023-05-13 01:08:09 +0200

    Delete Bogus-master directory

--- Bogus-master/Source/Bogus/Premium/License.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Bogus.Premium
-{
-   public class License
-   {
-      public static string LicenseTo { get; set; }
-      public static string LicenseKey { get; set; }
-   }
-}
--- Bogus-master/build.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-cls
-
-SET BUILD_ROOT=.
-
-dotnet tool restore
-
-dotnet nuke %1 --root "%BUILD_ROOT%"
